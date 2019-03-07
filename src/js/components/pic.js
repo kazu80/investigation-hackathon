0a7e56e9db681004d2c004bce8ba26753c0836f6
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2019-03-08 07:46:16 +0900

    styled components

--- src/js/components/pic.js
+++ src/js/components/pic.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Pic extends React.Component {
     render () {
         return (
-            <img src="/images/pic01.png" alt=""/>
+            <img src={this.props.path} alt=""/>
         );
     }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pic from './components/pic';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+display: flex;
+`;
+
 
 class Application extends React.Component {
     render () {
         return (
             <div>
                 <h1>Hello World</h1>
-                <Pic />
+                <Wrapper>
+                    <Pic path={'/images/pic01.png'} />
+                    <Pic path={'/images/pic02.png'} />
+                    <Pic path={'/images/pic03.png'} />
+                    <Pic path={'/images/pic04.png'} />
+                </Wrapper>
             </div>
         );
     }
