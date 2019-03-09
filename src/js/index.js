c528f952d90bd5aa2a63d3b32c3061098502cfc2
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2019-03-09 10:01:12 +0900

    infinite loop

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pic from './components/pic';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
-const Wrapper = styled.div`
+const Mask = styled.div`
 display: flex;
+flex-direction: row;
+margin: 0 auto;
+width: 200px;
+overflow: hidden;
+`;
+
+const slide01 = keyframes`
+from {
+transform: translateX(100%);
+}
+
+to {
+transform: translateX(-100%);
+}
+`;
+
+const slide02 = keyframes`
+from {
+transform: translateX(0);
+}
+
+to {
+transform: translateX(-200%);
+}
+`;
+
+const Wrapper01 = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+animation: ${slide01} 3s -1.5s linear infinite;
+`;
+
+const Wrapper02 = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+animation: ${slide02} 3s linear infinite;
 `;
 
 
@@ -13,12 +51,20 @@ class Application extends React.Component {
         return (
             <div>
                 <h1>Hello World</h1>
-                <Wrapper>
-                    <Pic path={'/images/pic01.png'} />
-                    <Pic path={'/images/pic02.png'} />
-                    <Pic path={'/images/pic03.png'} />
-                    <Pic path={'/images/pic04.png'} />
-                </Wrapper>
+                <Mask>
+                    <Wrapper01>
+                        <Pic path={'/images/pic01.png'} />
+                        <Pic path={'/images/pic02.png'} />
+                        <Pic path={'/images/pic03.png'} />
+                        <Pic path={'/images/pic04.png'} />
+                    </Wrapper01>
+                    <Wrapper02>
+                        <Pic path={'/images/pic01.png'} />
+                        <Pic path={'/images/pic02.png'} />
+                        <Pic path={'/images/pic03.png'} />
+                        <Pic path={'/images/pic04.png'} />
+                    </Wrapper02>
+                </Mask>
             </div>
         );
     }
