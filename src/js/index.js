0e156125d29f44ea3b734be8375ebec36df4283a
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2019-03-09 10:39:04 +0900

    infinite loop custom

--- src/js/index.js
+++ src/js/index.js
@@ -12,37 +12,128 @@ overflow: hidden;
 `;
 
 const slide01 = keyframes`
-from {
+0% {
 transform: translateX(100%);
 }
 
-to {
+25% {
+transform: translateX(50%);
+}
+
+37.5% {
+transform: translateX(25%);
+}
+
+49% {
+transform: translateX(25%);
+}
+
+50% {
+transform: translateX(0%);
+}
+
+61.5% {
+transform: translateX(0%);
+}
+
+62.5% {
+transform: translateX(-25%);
+}
+
+74% {
+transform: translateX(-25%);
+}
+
+75% {
+transform: translateX(-50%);
+}
+
+86.5% {
+transform: translateX(-50%);
+}
+
+87.5% {
+transform: translateX(-75%);
+}
+
+99% {
+transform: translateX(-75%);
+}
+
+100% {
 transform: translateX(-100%);
 }
 `;
 
 const slide02 = keyframes`
-from {
+0% {
 transform: translateX(0);
 }
 
-to {
+25% {
+transform: translateX(-50%);
+}
+
+75% {
+transform: translateX(-75%);
+}
+
+49% {
+transform: translateX(-75%);
+}
+
+50% {
+transform: translateX(-100%);
+}
+
+61.5% {
+transform: translateX(-100%);
+}
+
+62.5% {
+transform: translateX(-125%);
+}
+
+74% {
+transform: translateX(-125%);
+}
+
+75% {
+transform: translateX(-150%);
+}
+
+86.5% {
+transform: translateX(-150%);
+}
+
+87.5% {
+transform: translateX(-175%);
+}
+
+99% {
+transform: translateX(-175%);
+}
+
+100% {
 transform: translateX(-200%);
 }
 `;
+
+const AnimationTime = 2000;
+const AnimationUnit = 'ms';
 
 const Wrapper01 = styled.div`
 display: flex;
 flex-direction: row;
 justify-content: center;
-animation: ${slide01} 3s -1.5s linear infinite;
+animation: ${slide01} ${AnimationTime + AnimationUnit} ${((AnimationTime / 2) * -1) + AnimationUnit} linear infinite;
 `;
 
 const Wrapper02 = styled.div`
 display: flex;
 flex-direction: row;
 justify-content: center;
-animation: ${slide02} 3s linear infinite;
+animation: ${slide02} ${AnimationTime + AnimationUnit} linear infinite;
 `;
 
 
