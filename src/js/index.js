21bcd81ee5caab54036dcdf48c86fbbde70ad75d
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2019-03-13 06:47:03 +0900

    React router

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { Home } from './components/home';
+import { Login } from './components/login';
+import { Search } from './components/search';
 
 class Application extends React.Component {
     render () {
         return (
             <BrowserRouter>
                 <div>
-                    <Route exact path='/' component={Home} />
+                    <Route exact path='/' component={Login} />
+                    <Route exact path='/home' component={Home} />
+                    <Route exact path='/search' component={Search} />
                 </div>
             </BrowserRouter>
         );
