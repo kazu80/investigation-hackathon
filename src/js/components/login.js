8a44a1364b70fa84405d37eac729193edfb8b3e8
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2019-03-13 07:23:35 +0900

    coding login

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export class Login extends React.Component {
     render () {
         return (
-            <div></div>
+            <Wrapper>
+                <Panel>
+                    <Title>INVESTIGATION</Title>
+                    <Button href="#">LOGIN</Button>
+                </Panel>
+            </Wrapper>
         );
     }
 }
+
+const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 100%;
+background: url("/images/wall01.jpg") no-repeat center center;
+background-size: cover;
+`;
+
+const Panel = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+padding-top: 92px;
+padding-bottom: 160px;
+width: 700px;
+height: 400px;
+background: linear-gradient(45deg, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 100%);
+`;
+
+const Title = styled.div`
+display: inline-block;
+width: 100%;
+font-size: 30px;
+line-height: 100%;
+color: #707070;
+text-align: center;
+`;
+
+const Button = styled.a`
+display: block;
+position: relative;
+width: 400px;
+height: 80px;
+font-size: 30px;
+line-height: 80px;
+color: #fff;
+background-color: #4469B0;
+text-decoration: none;
+text-align: center;
+outline: none;
+
+&:before {
+content: "";
+position: absolute;
+top: 50%;
+left: 30px;
+transform: translateY(-50%);
+width: 45px;
+height: 45px;
+background: url("/images/icon-fb.svg") center no-repeat;
+background-size: contain;
+}
+`;
